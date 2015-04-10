de38dc9de3b71fef8aabefcd5fb53ca9642383f2
r33p <pierre@314r.net>
2015-04-10 11:07:29 +0200

    nettoyage de choses inutiles, ré-affectations de chemins non trouvés

--- LIVE.sh
+++ LIVE.sh
@@ -1,1 +1,3 @@
-puredata -rt -sleepgrain 1 -noaudio -alsamidi /home/rep/2015_VJSet/LIVE.pd 
+modprobe v4l2loopback &&
+ffmpeg -f x11grab -r 25 -s 256x256 -follow_mouse centered -show_region 1 -i :0.0+0,0 -vcodec rawvideo -pix_fmt yuv420p -threads 0 -f v4l2 /dev/video0 &&
+puredata -rt -sleepgrain 1 -noaudio -alsamidi ./LIVE.pd 
